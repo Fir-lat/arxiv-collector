71911963afb44c1b8f7b79f3b78e087b2156a0c7
李恒庄 <1724381733@qq.com>
2025-04-06 11:20:22 +0800

    env

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  # 加载环境变量
+
 class Config:
-    DEEPSEEK_API_KEY = "<Place Your API Key Here>"
+    DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
     BASE_PATH = os.path.expanduser("../arxiv_collection") # Where your arxiv-collection is going to be
     CATEGORIES = ["cs.AI", "cs.CV", "cs.LG"]
     TARGET_TOPICS = [
@@ -17,4 +21,4 @@ class Config:
 if __name__ == '__main__':
     config = Config()
     os.makedirs(config.BASE_PATH, exist_ok=True)
-    print(config.BASE_PATH)
+    print(config.DEEPSEEK_API_KEY)
